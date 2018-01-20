a9d55013baf0f20f3b946abe627d5a72b9e17af8
dingzhong <dingzhong55555@gmail.com>
2018-01-20 21:11:13 +0800

    add paginate function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true
+  # scope 将数据库的查询方式封装成一个可复用的方法
+  scope :recent, -> { order("created_at DESC") }
 
   belongs_to :user
   belongs_to :group
