e3eb2a73b149bc720a7e5bc3fb293f74326b9841
dingzhong <dingzhong55555@gmail.com>
2018-01-20 20:46:46 +0800

    add post new function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,14 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
-  
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def new
@@ -15,6 +17,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
@@ -24,12 +27,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -38,7 +38,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
 
     redirect_to groups_path
@@ -46,6 +45,15 @@ class GroupsController < ApplicationController
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path
+      flash[:notice] = "You have no permission"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116165329) do
+ActiveRecord::Schema.define(version: 20180120111421) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
